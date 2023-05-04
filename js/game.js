6b0f3e88e74d2417513e2213b163ec372ebb5d23
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-05-04 17:19:53 -0300

    feat: create function playRound

--- js/game.js
+++ js/game.js
@@ -1,14 +1,39 @@
-function getComputerChoice(max) {
-  const result = Math.floor(Math.random() * max)
+function getComputerChoice() {
+  const result = Math.floor(Math.random() * 3)
 
   switch (result) {
     case 0:
-      return 'Rock'
+      return 'rock'
     case 1:
-      return 'Paper'
+      return 'paper'
     case 2:
-      return 'Scissors'
+      return 'scissors'
   }
 }
 
-console.log(getComputerChoice(3))
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toUpperCase()
+  const computer = computerSelection.toUpperCase()
+
+  const capitalizedPlayerSelection =
+    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+
+  const capitalizedComputerSelection =
+    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+
+  if (player === 'ROCK' && computer === 'SCISSORS') {
+    return `You win! ${capitalizedPlayerSelection} beats ${computerSelection}!`
+  } else if (player === 'PAPER' && computer === 'ROCK') {
+    return `You win! ${capitalizedPlayerSelection} beats ${computerSelection}!`
+  } else if (player === 'SCISSORS' && computer === 'PAPER') {
+    return `You win! ${capitalizedPlayerSelection} beats ${computerSelection}!`
+  } else if (player === computer) {
+    return 'Tie!'
+  } else {
+    return `You Lose! ${capitalizedComputerSelection} beats ${playerSelection}!`
+  }
+}
+
+const playerSelection = 'rock'
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
