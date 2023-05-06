16f8c317de2530d890227a7e3cff1466c972246e
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-05-06 18:33:00 -0300

    fix: change game, playRound and getComputerChoice

--- js/game.js
+++ js/game.js
@@ -1,45 +1,10 @@
-function getComputerChoice() {
-  const result = Math.floor(Math.random() * 3)
-
-  switch (result) {
-    case 0:
-      return 'rock'
-    case 1:
-      return 'paper'
-    case 2:
-      return 'scissors'
-  }
-}
-
-function playRound(playerSelection, computerSelection) {
-  const player = playerSelection.toUpperCase()
-  const computer = computerSelection.toUpperCase()
-
-  const capitalizedPlayerSelection =
-    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-
-  const capitalizedComputerSelection =
-    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-
-  if (player === 'ROCK' && computer === 'SCISSORS') {
-    return `You win! ${capitalizedPlayerSelection} beats ${computerSelection}!`
-  } else if (player === 'PAPER' && computer === 'ROCK') {
-    return `You win! ${capitalizedPlayerSelection} beats ${computerSelection}!`
-  } else if (player === 'SCISSORS' && computer === 'PAPER') {
-    return `You win! ${capitalizedPlayerSelection} beats ${computerSelection}!`
-  } else if (player === computer) {
-    return 'Tie!'
-  } else {
-    return `You Lose! ${capitalizedComputerSelection} beats ${playerSelection}!`
-  }
-}
+import { playRound, getComputerChoice } from './utils.js'
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Rock, paper or scissors:')
+    const playerSelection = 1
     const computerSelection = getComputerChoice()
-    const winner = playRound(playerSelection, computerSelection)
-    alert(winner)
+    playRound(playerSelection, computerSelection)
   }
 }
 
---
+++ js/utils.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3)
+}
+
+function playRound(playerSelection, computerSelection) {
+  console.log('entrou')
+}
+
+export { getComputerChoice, playRound }
