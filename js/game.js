7f0e35b7db394a80932c2a3a0c1af6ecea89aea4
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-05-07 03:19:49 -0300

    fix: functions, css and images

--- js/game.js
+++ js/game.js
@@ -1,3 +1,7 @@
+let countRound = 1
+let playerLives = 5
+let enemyLives = 5
+
 function getComputerChoice() {
   return Math.floor(Math.random() * 3)
 }
@@ -8,18 +12,108 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === 1 && computerSelection === 0) ||
     (playerSelection === 2 && computerSelection === 3)
   ) {
-    return 'Winner: player'
+    return changeGameStats('player', playerSelection, computerSelection)
   } else if (playerSelection === computerSelection) {
-    return 'Tie'
+    return changeGameStats('tie', playerSelection, computerSelection)
   } else {
-    return 'Winner: computer'
+    return changeGameStats('computer', playerSelection, computerSelection)
   }
+}
+
+function showEnemyStats(computerSelection, countRound) {
+  const enemy = document.getElementById('enemy')
+  const round = document.getElementById('round')
+  round.innerText = `${countRound}`
+
+  for (let node of enemy.children) {
+    if (node.src !== undefined) {
+      if (computerSelection === 0) {
+        node.src = './assets/images/fire.png'
+        node.style.width = '80px'
+      } else if (computerSelection === 1) {
+        node.src = './assets/images/ice.png'
+        node.style.width = '80px'
+      } else {
+        node.src = './assets/images/fire.png'
+        node.style.width = '80px'
+      }
+    } else {
+      node.innerText = "Enemy's power"
+    }
+  }
+}
+
+function changeGameStats(winner, playerSelection, computerSelection) {
+  const enemyCurrentLives = document.getElementById('enemy-lives')
+  const playerCurrentlives = document.getElementById('player-lives')
+  const combatArea = document.getElementById('message')
+
+  if (winner === 'player') {
+    enemyLives -= 1
+    enemyCurrentLives.innerText = `${enemyLives}`
+    playerWinner(playerSelection, combatArea)
+  } else if (winner === 'computer') {
+    playerLives -= 1
+    playerCurrentlives.innerText = `${playerLives}`
+    enemyWinner(computerSelection, combatArea)
+  } else {
+    combatArea.innerText = 'Nice fight but...tie'
+  }
+}
+
+function playerWinner(playerSelection, combatArea) {
+  if (playerSelection === 0) {
+    combatArea.innerText = "Should't let your guard down!"
+  } else if (playerSelection === 1) {
+    combatArea.innerText = 'Kamisato Art... Soumetsu!'
+  } else {
+    combatArea.innerText = 'Pyre pyre pants on fire!'
+  }
+}
+
+function enemyWinner(computerSelection, combatArea) {
+  if (computerSelection === 0) {
+    combatArea.innerText = 'Boya lata!'
+  } else if (computerSelection === 1) {
+    combatArea.innerText = 'Lata Boya Sada!'
+  } else {
+    combatArea.innerText = 'Celi Lata!'
+  }
+}
+
+function endGame() {
+  const main = document.querySelector('main')
+  const header = document.querySelector('header')
+  const button = document.createElement('button')
+  if (enemyLives === 0 || playerLives === 0) {
+    while (main.firstChild) {
+      main.removeChild(main.firstChild)
+    }
+
+    if (enemyLives > 0) {
+      header.children[1].innerText = 'YOU LOST THE GAME!'
+    } else {
+      header.children[1].innerHTML = 'YOU WON THE GAME!'
+    }
+
+    button.innerText = 'Restar'
+    button.className = 'btn'
+    header.appendChild(button)
+    button.addEventListener('click', restartGame)
+  }
+}
+
+function restartGame() {
+  window.location.reload()
 }
 
 function game(e) {
   const playerSelection = Number(e.target.dataset.key)
   const computerSelection = getComputerChoice()
+  showEnemyStats(computerSelection, countRound)
   playRound(playerSelection, computerSelection)
+  countRound++
+  endGame()
 }
 
 function togglePower() {
@@ -31,16 +125,16 @@ function togglePower() {
 
       switch (key) {
         case 0:
-          e.target.src = './assets/images/water-2.png'
-          e.target.style.width = '90px'
+          e.target.src = './assets/images/tartas2.png'
+          e.target.style.width = '130px'
           break
         case 1:
-          e.target.src = './assets/images/snow-2.png'
-          e.target.style.width = '90px'
+          e.target.src = './assets/images/ayaka2.png'
+          e.target.style.width = '110px'
           break
         case 2:
-          e.target.src = './assets/images/fire-2.png'
-          e.target.style.width = '90px'
+          e.target.src = './assets/images/hutao2.png'
+          e.target.style.width = '160px'
           break
       }
     })
@@ -50,16 +144,16 @@ function togglePower() {
 
       switch (key) {
         case 0:
-          e.target.src = './assets/images/water-1.png'
-          e.target.style.width = '80px'
+          e.target.src = './assets/images/tartas1.png'
+          e.target.style.width = '120px'
           break
         case 1:
-          e.target.src = './assets/images/snow-1.png'
-          e.target.style.width = '80px'
+          e.target.src = './assets/images/ayaka1.png'
+          e.target.style.width = '100px'
           break
         case 2:
-          e.target.src = './assets/images/fire-1.png'
-          e.target.style.width = '80px'
+          e.target.src = './assets/images/hutao1.png'
+          e.target.style.width = '120px'
           break
       }
     })
