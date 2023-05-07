51193e83239b5ddc6ff257ab2f8adfa3ba202f8d
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-05-07 00:02:58 -0300

    feat: add new function togglePower and change old functions

--- js/game.js
+++ js/game.js
@@ -1,11 +1,69 @@
-import { playRound, getComputerChoice } from './utils.js'
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3)
+}
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = 1
-    const computerSelection = getComputerChoice()
-    playRound(playerSelection, computerSelection)
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === 0 && computerSelection === 2) ||
+    (playerSelection === 1 && computerSelection === 0) ||
+    (playerSelection === 2 && computerSelection === 3)
+  ) {
+    return 'Winner: player'
+  } else if (playerSelection === computerSelection) {
+    return 'Tie'
+  } else {
+    return 'Winner: computer'
   }
 }
 
-game()
+function game(e) {
+  const playerSelection = Number(e.target.dataset.key)
+  const computerSelection = getComputerChoice()
+  playRound(playerSelection, computerSelection)
+}
+
+function togglePower() {
+  const powers = document.getElementById('powers')
+
+  for (const power of powers.children) {
+    power.addEventListener('mouseover', e => {
+      const key = Number(e.target.getAttribute('data-key'))
+
+      switch (key) {
+        case 0:
+          e.target.src = './assets/images/water-2.png'
+          e.target.style.width = '90px'
+          break
+        case 1:
+          e.target.src = './assets/images/snow-2.png'
+          e.target.style.width = '90px'
+          break
+        case 2:
+          e.target.src = './assets/images/fire-2.png'
+          e.target.style.width = '90px'
+          break
+      }
+    })
+
+    power.addEventListener('mouseout', e => {
+      const key = Number(e.target.getAttribute('data-key'))
+
+      switch (key) {
+        case 0:
+          e.target.src = './assets/images/water-1.png'
+          e.target.style.width = '80px'
+          break
+        case 1:
+          e.target.src = './assets/images/snow-1.png'
+          e.target.style.width = '80px'
+          break
+        case 2:
+          e.target.src = './assets/images/fire-1.png'
+          e.target.style.width = '80px'
+          break
+      }
+    })
+  }
+}
+
+togglePower()
--- js/utils.js
+++
@@ -1,9 +0,0 @@
-function getComputerChoice() {
-  return Math.floor(Math.random() * 3)
-}
-
-function playRound(playerSelection, computerSelection) {
-  console.log('entrou')
-}
-
-export { getComputerChoice, playRound }
